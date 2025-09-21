d53bde64ec57b59bce3b51ebfad7f399bb124bab
Abhishek Prajapat <abhi03prajapat@gmail.com>
2025-09-21 15:26:52 +0530

    Install react-icons and update dependencies

--- artist-backend/server.js
+++ artist-backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5000", credentials: true }));
 app.use(express.json());
 
 // Routes
